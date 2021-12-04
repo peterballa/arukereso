a98f7463bdfd31f032449e80bdf09fed966c0f90
ballapeter <ballapeter@sitefactory.hu>
2021-12-04 14:17:45 +0100

    Order and OrderProduct models are added.

---
+++ app/Models/Order.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Order extends Model
+{
+    protected $table = 'order';
+
+    protected $casts = [
+        'id' => 'integer',
+        'name' => 'string',
+        'email' => 'string',
+        'delivery_mode' => 'string',
+        'invoice_name' => 'string',
+        'invoice_zip_code' => 'string',
+        'invoice_city' => 'string',
+        'invoice_address' => 'string',
+        'delivery_name' => 'string',
+        'delivery_zip_code' => 'string',
+        'delivery_city' => 'string',
+        'delivery_address' => 'string',
+        'status' => 'string'
+    ];
+
+    public function setName(string $name): Order
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function setEmail(string $email): Order
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function setDeliveryMode(string $deliveryMode): Order
+    {
+        $this->delivery_mode = $deliveryMode;
+
+        return $this;
+    }
+
+    public function setInvoiceName(string $invoiceName): Order
+    {
+        $this->invoiceName = $invoiceName;
+
+        return $this;
+    }
+
+    public function setInvoiceZipCode(string $invoiceZipCode): Order
+    {
+        $this->invoice_zip_code = $invoiceZipCode;
+
+        return $this;
+    }
+
+    public function setInvoiceCity(string $invoiceCity): Order
+    {
+        $this->invoice_city = $invoiceCity;
+
+        return $this;
+    }
+
+    public function setInvoiceAddress(string $invoiceAddress): Order
+    {
+        $this->invoice_address = $invoiceAddress;
+
+        return $this;
+    }
+
+    public function setDeliveryName(string $deliveryName): Order
+    {
+        $this->deliveryName = $deliveryName;
+
+        return $this;
+    }
+
+    public function setDeliveryZipCode(string $deliveryZipCode): Order
+    {
+        $this->delivery_zip_code = $deliveryZipCode;
+
+        return $this;
+    }
+
+    public function setDeliveryCity(string $deliveryCity): Order
+    {
+        $this->delivery_city = $deliveryCity;
+
+        return $this;
+    }
+
+    public function setDeliveryAddress(string $deliveryAddress): Order
+    {
+        $this->delivery_address = $deliveryAddress;
+
+        return $this;
+    }
+
+    public function setStatus(string $status): Order
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function orderProducts(): HasMany
+    {
+        return $this->hasMany(OrderProduct::class);
+    }
+
+    public function getOrderProducts(): Collection
+    {
+        return $this->orderProducts;
+    }
+}
---
+++ app/Models/OrderProduct.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class OrderProduct extends Model
+{
+    protected $table = 'order_product';
+
+    protected $casts = [
+        'id' => 'integer',
+        'order_id' => 'integer',
+        'name' => 'string',
+        'gross_price' => 'integer',
+        'quantity' => 'integer',
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s'
+    ];
+
+    public function setOrderId(int $orderId): OrderProduct
+    {
+        $this->order_id = $orderId;
+
+        return $this;
+    }
+
+    public function setName(string $name): OrderProduct
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function setGrossPrice(int $grossPrice): OrderProduct
+    {
+        $this->gross_price = $grossPrice;
+
+        return $this;
+    }
+
+    public function setQuantity(int $quantity): OrderProduct
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function getOrder(): Order
+    {
+        return $this->order;
+    }
+}
